6861be30fd58a4e2866f7672f45feb41d762b900
Lee Chu Yem <leechuyem@gmail.com>
2019-09-12 20:23:03 +1000

    added goToSpecificScene script

---
+++ Assets/Code/Scripts/GoToSpecificScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class GoToSpecificScene : MonoBehaviour
+{
+    public string sceneName; 
+    private void OnMouseUp() {
+        goToScene(); 
+    }
+    public void goToScene() {
+        SceneManager.LoadScene (sceneName);
+    }
+}
