f72c72ac83999af53f2f1da93171645a52ea8f73
Lee Chu Yem <leechuyem@gmail.com>
2019-09-12 14:22:54 +1000

    removed collision between fixedfood and draggablefood

---
+++ Assets/Code/Enums/Layers.cs
@@ -0,0 +1,12 @@
+enum Layers {
+    DEFAULT,
+    TRANSPARENTFX,
+    IGNORERAYCAST,
+    LAYER3,
+    WATER,
+    UI,
+    LAYER6,
+    LAYER7,
+    FIXEDFOOD,
+    DRAGGABLEFOOD
+}
---
+++ Assets/Code/Scripts/Controllers/PhysicsController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics2D.IgnoreLayerCollision((int)Layers.FIXEDFOOD, (int)Layers.DRAGGABLEFOOD, true);
+        Physics2D.IgnoreLayerCollision((int)Layers.UI, (int)Layers.DRAGGABLEFOOD, true);        
+    }
+
+}
