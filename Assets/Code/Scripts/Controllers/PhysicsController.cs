cddf9382bc345abe397fd469e756497df08d8014
Lee Chu Yem <leechuyem@gmail.com>
2019-09-12 14:23:57 +1000

    removed starter comments

--- Assets/Code/Scripts/Controllers/PhysicsController.cs
+++ Assets/Code/Scripts/Controllers/PhysicsController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PhysicsController : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         Physics2D.IgnoreLayerCollision((int)Layers.FIXEDFOOD, (int)Layers.DRAGGABLEFOOD, true);
